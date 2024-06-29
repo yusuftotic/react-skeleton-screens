3ec24e085a075803ce008a4822e4a80d12495e73
yusuftotic <yusuftotic@outlook.com>
2024-06-29 18:26:00 +0300

    Add data fetching with fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import Articles from './components/Articles';
+import User from './components/User';
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+
+      <header>
+        <h1>React Skeletons</h1>
+      </header>
+
+      <div className="content">
+        <Articles />
+        <User />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
   )
 }
-
-export default App
---
+++ src/components/User.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+
+export default function User() {
+
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+
+    setTimeout(async () => {
+
+      const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
+
+      const data = await res.json();
+
+      setProfile(data);
+
+    }, 5000);
+
+  }, []);
+
+  return (
+    <div className="user">
+
+      <h2>User Details</h2>
+
+      {
+        profile && (
+          <div className="profile">
+
+            <h3>{profile.username}</h3>
+            <p>{profile.email}</p>
+            <a href={profile.website}>{profile.website}</a>
+
+          </div>
+        )
+      }
+
+      {!profile && <div>Loading...</div>}
+
+    </div>
+
+  )
+}
